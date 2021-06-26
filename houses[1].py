bf3f4c8264e6798facb6725f7f893650cc7055a9
ethangillett <86503149+ethangillett@users.noreply.github.com>
2021-06-26 14:00:24 -0600

    Add files via upload

--- houses[1].py
+++ houses[1].py
@@ -5,20 +5,18 @@ class HousesSpider(scrapy.Spider):
     name = 'houses'
     allowed_domains = ['och.byu.edu']
     start_urls = ['https://och.byu.edu/all-facilities']
+    custom_settings = {
+        'FEED_FORMAT':'csv',
+        'FEED_URI':'test.csv'
+    }
 
     def parse(self, response):
         
         table = response.xpath('//*[@id="myTable"]')
 
-        for i in range(0,len(response.xpath("//*[@id='myTable']/tr[*]").getall()),2):
-            apt_complexlist = response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").getall()
-            apt_complex = []
-            #Attempting to remove the 1st, 3rd, and 5th, element 
-            #for i in range(0,len(response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").getall())):
-                apt_complex.append(i)
-                if ("\r\n"):
-                    list.remove
-            print(apt_complexlist) 
-            
-
-        
+        for i in range(0,len(response.xpath("//*[@id='myTable']/tr[*]").extract()),2):
+            apt_complexlist = response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").extract()
+            apt_complex = yield {
+                'apt_complexlist':response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").getall()
+            }
+       
