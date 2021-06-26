7cd54bafcde1310a508cfb71170bd196cd5b4355
ethangillett <86503149+ethangillett@users.noreply.github.com>
2021-06-26 14:28:42 -0600

    Add files via upload

--- houses[1].py
+++ houses[1].py
@@ -5,18 +5,12 @@ class HousesSpider(scrapy.Spider):
     name = 'houses'
     allowed_domains = ['och.byu.edu']
     start_urls = ['https://och.byu.edu/all-facilities']
-    custom_settings = {
-        'FEED_FORMAT':'csv',
-        'FEED_URI':'test.csv'
-    }
-
+    
     def parse(self, response):
-        
-        table = response.xpath('//*[@id="myTable"]')
 
         for i in range(0,len(response.xpath("//*[@id='myTable']/tr[*]").extract()),2):
-            apt_complexlist = response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").extract()
             apt_complex = yield {
                 'apt_complexlist':response.xpath("//*[@id='myTable']/tr[" + str(i) + "]//text()").getall()
             }
+
        
